f5520f8e542db27f79408e55d729c9b35b877240
Andrey Mena Espinoza <andrey.menaespinoza@ucr.ac.cr>
2024-04-10 13:44:38 -0600

    Despliegue de texto

--- Lab04_sdl_engine_00/src/game/Game.cpp
+++ Lab04_sdl_engine_00/src/game/Game.cpp
@@ -16,6 +16,11 @@ void Game::init() {
 		return;
 	}
 
+	if (TTF_Init() < 0) {
+		std::cout << "Error al inicializar SDL_TTF" << std::endl;
+		return;
+	}
+
 	this->windowWidth= 800;
 	this->windowHeight = 600;
 
@@ -36,7 +41,39 @@ void Game::init() {
 		0
 	);
 
+	// Cargar texto
+	this->fontSize = 12;
+	this->font = TTF_OpenFont("./assets/fonts/press_start_2p.ttf",
+		this->fontSize);
+
 	this->isRunning = true;
+
+	// Inicializar datos de la imagen
+	this->imgWidth = 32;
+	this->imgHeight = 32;
+	this->pos.x = (this->windowWidth / 2) - (this->imgWidth / 2);
+	this->pos.y = (this->windowHeight / 2) - (this->imgHeight / 2);
+	SDL_Surface* imgSurface = IMG_Load("./assets/images/skull_00.png");
+	this->imgTexture = SDL_CreateTextureFromSurface(this->renderer, imgSurface);
+	SDL_FreeSurface(imgSurface);
+	this->srcRect.x = 0;
+	this->srcRect.x = 0;
+	this->srcRect.w = this->imgWidth;
+	this->srcRect.h = this->imgHeight;
+
+	// Inicializardatos del texto
+	this->message = "Lab 04: Intro al motor de videojuegos";
+	this->fontColor.r = 255;
+	SDL_Surface* txtSurface = TTF_RenderText_Solid(this->font,
+		this->message.c_str(),
+		this->fontColor
+	);
+	this->txtTexture = SDL_CreateTextureFromSurface(this->renderer, txtSurface);
+	this->txtWidth = txtSurface->w;
+	this->txtHeight = txtSurface->h;
+	this->txtPos.x = (this->windowWidth / 2) - (this->txtWidth / 2);
+	this->txtPos.y = 20;
+	SDL_FreeSurface(txtSurface);
 }
 
 void Game::processInput() {
@@ -61,6 +98,43 @@ void Game::processInput() {
 void Game::render() {
 	SDL_SetRenderDrawColor(this->renderer, 30, 30, 30, 255);
 	SDL_RenderClear(this->renderer);
+
+	SDL_Rect imgDstRect = {
+		this->pos.x,
+		this->pos.y,
+		this->imgWidth,
+		this->imgHeight
+	};
+
+	SDL_Rect txtDstRect = {
+	this->txtPos.x,
+	this->txtPos.y,
+	this->txtWidth,
+	this->txtHeight
+	};
+
+	// Dibujar imagen
+	// https://wiki.libsdl.org/SDL2/SDL_RenderCopyEx
+	SDL_RenderCopyEx(
+		this->renderer,
+		this->imgTexture,
+		&this->srcRect,
+		&imgDstRect,
+		this->angle,
+		NULL,
+		SDL_FLIP_NONE
+	);
+
+	SDL_RenderCopyEx(
+		this->renderer,
+		this->txtTexture,
+		NULL, // Si es NULL dibuja toda la textura
+		&txtDstRect,
+		this->txtAngle,
+		NULL,
+		SDL_FLIP_NONE
+	);
+
 	SDL_RenderPresent(this->renderer);
 }
 
@@ -73,7 +147,12 @@ void Game::run() {
 }
 
 void Game::destroy() {
+	SDL_DestroyTexture(this->txtTexture);
+	SDL_DestroyTexture(this->imgTexture);
+
 	SDL_DestroyRenderer(this->renderer);
 	SDL_DestroyWindow(this->window);
+
+	TTF_Quit();
 	SDL_Quit();
 }
--- Lab04_sdl_engine_00/src/game/Game.h
+++ Lab04_sdl_engine_00/src/game/Game.h
@@ -5,19 +5,36 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
+#include <string>
 
 class Game {
 	SDL_Window* window = nullptr;
 	SDL_Renderer* renderer = nullptr;
-	
+
 	size_t windowWidth = 0;
 	size_t windowHeight = 0;
+	
+	TTF_Font* font = nullptr;
+	int fontSize = 0;
+	SDL_Color fontColor= { 0, 0, 0 };
 
 	bool isRunning = false;
 
 	// Atributos de la imagen
 	SDL_Texture* imgTexture = nullptr;
+	glm::vec2 pos = glm::vec2();
+	size_t imgWidth = 0;
+	size_t imgHeight = 0;
+	SDL_Rect srcRect = { 0, 0, 0, 0 };
+	double angle = 0.0;
 
+	// Atributos del texto
+	SDL_Texture* txtTexture = nullptr;
+	std::string message = "";
+	glm::vec2 txtPos = glm::vec2();
+	size_t txtWidth = 0;
+	size_t txtHeight = 0;
+	double txtAngle= 0.0;
 
 	void processInput();
 	void update();
